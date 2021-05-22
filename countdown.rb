3e5615773201a20093b144b85ae646cc16296f84
Utshant Gurung <utshantgurung@gmail.com>
2021-05-22 12:47:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,5 @@
 #write your code here
 
-def countdown_with_sleep(count)
-  sleep(1.second)
-end
-
 def countdown(number)
   while number >= 1
     puts "#{number} SECOND(S)!"
@@ -13,3 +9,6 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(sleep)
+  sleep(1.second)
+end
